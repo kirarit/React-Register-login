ba873affca61dea7b3e90a47e0d0d3dfd2d7c4b7
David Kirarit <davidkrarit@gmail.com>
2021-05-23 23:11:12 +0300

    login, register components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js file
+import React, { Component } from "react";
+import "./App.css";
+import Signup from "./components/signUp/Signup";
+import Signin from "./components/SignIn/Signin";
+import Home from "./components/Home/Home";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    let navLink = (
+      <div className="Tab">
+        <NavLink to="/" activeClassName="activeLink" className="signIn">
+          Sign In
+        </NavLink>
+        <NavLink exact to="/register" activeClassName="activeLink" className="signUp">
+          Sign Up
+        </NavLink>
+      </div>
+    );
+    const login = localStorage.getItem("isLoggedIn");
+
+    return (
+      <div className="App">
+        {login ? (
+          <Router>
+            <Route exact path="/" component={Signin}></Route>
+            <Route path="/register" component={Signup}></Route>
+            <Route path="/home" component={Home}></Route>
+          </Router>
+        ) : (
+          <Router>
+            {navLink}
+            <Route exact path="/" component={Signin}></Route>
+            <Route path="/register" component={Signup}></Route>
+            <Route path="/home" component={Home}></Route>
+          </Router>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
